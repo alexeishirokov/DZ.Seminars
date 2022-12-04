3df54f993dcea335d6ca8f01a9c08d95a72af6fd
Alexey <shiry-1980@mail.ru>
2022-12-04 13:02:39 +0300

    сделано task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"Введите первое число: ");
+
+ int a = int.Parse(Console.ReadLine());
+
+ Console.WriteLine($"Введите второе число: ");
+
+   int b = int.Parse(Console.ReadLine());
+
+    if(a > b){
+
+       Console.WriteLine($"{a} больше чем {b}");
+   }
+     else{
+
+      Console.WriteLine($"{b} больше чем {a}");
+     }
